c43a2d514a902cd595a6c1a67ac34ad6855eccb7
DenisPuliaev <denis.puliaev@gmail.com>
2021-02-05 19:53:51 +0200

    small fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import './styles/main.scss';
-import './App.scss';
 import FormPage from './pages/FormPage';
 
 function App() {
--- src/pages/FormPage/FormPage.js
+++ src/pages/FormPage/FormPage.js
@@ -6,133 +6,135 @@ import './form-page.scss';
 
 function Form() {
   return (
-    <Container className="form-page" style={{background: '#a8a17d'}}>
-      <FormGroup className="form-page__genders-row">
-        <Row className="form-page__row">
-          <Col xs={4}>
-            <FormLabel className="form-page__label">
-              Anrede*
-            </FormLabel>
-          </Col>
-          <Col xs={8} md={6}>
-            <div className="form-page__inputs">
-              <Input type="radio" className="form-page__gender">Herr</Input>
-              <Input type="radio" className="form-page__gender">Frau</Input>
-            </div>
-          </Col>
-        </Row>
-      </FormGroup>
+    <Container className="form-page">
+      <form>
+        <FormGroup className="form-page__genders-row">
+          <Row className="form-page__row">
+            <Col xs={4}>
+              <FormLabel className="form-page__label">
+                Anrede*
+              </FormLabel>
+            </Col>
+            <Col xs={8} md={6}>
+              <div className="form-page__inputs">
+                <Input type="radio" name="gender" className="form-page__gender">Herr</Input>
+                <Input type="radio" name="gender" className="form-page__gender">Frau</Input>
+              </div>
+            </Col>
+          </Row>
+        </FormGroup>
 
-      <FormGroup>
-        <Row className="form-page__row">
-          <Col md={4}>
-            <FormLabel className="form-page__label">
-              Vorname*
-            </FormLabel>
-          </Col>
-          <Col md={6}>
-            <div className="form-page__inputs">
-              <Input placeholder="Vorname" />
-            </div>
-          </Col>
-        </Row>
-      </FormGroup>
+        <FormGroup>
+          <Row className="form-page__row">
+            <Col md={4}>
+              <FormLabel className="form-page__label">
+                Vorname*
+              </FormLabel>
+            </Col>
+            <Col md={6}>
+              <div className="form-page__inputs">
+                <Input placeholder="Vorname" />
+              </div>
+            </Col>
+          </Row>
+        </FormGroup>
 
-      <FormGroup>
-        <Row className="form-page__row">
-          <Col md={4}>
-            <FormLabel className="form-page__label">
-              Nachname*
-            </FormLabel>
-          </Col>
-          <Col md={6}>
-            <div className="form-page__inputs">
-              <Input placeholder="Nachname" />
-            </div>
-          </Col>
-        </Row>
-      </FormGroup>
+        <FormGroup>
+          <Row className="form-page__row">
+            <Col md={4}>
+              <FormLabel className="form-page__label">
+                Nachname*
+              </FormLabel>
+            </Col>
+            <Col md={6}>
+              <div className="form-page__inputs">
+                <Input placeholder="Nachname" />
+              </div>
+            </Col>
+          </Row>
+        </FormGroup>
 
-      <FormGroup>
-        <Row className="form-page__row">
-          <Col md={4}>
-            <FormLabel className="form-page__label">
-              Geburtsdatum*
-            </FormLabel>
-          </Col>
-          <Col md={6}>
-            <div className="form-page__inputs">
-              <Row>
-                <Col xs={4}>
-                  <Input placeholder="TT" />
-                </Col>
-                <Col xs={4}>
-                  <Input placeholder="MM" />
-                </Col>
-                <Col xs={4}>
-                  <Input placeholder="JJJJ"/>
-                </Col>
-              </Row>
-            </div>
-          </Col>
-        </Row>
-      </FormGroup>
+        <FormGroup>
+          <Row className="form-page__row">
+            <Col md={4}>
+              <FormLabel className="form-page__label">
+                Geburtsdatum*
+              </FormLabel>
+            </Col>
+            <Col md={6}>
+              <div className="form-page__inputs">
+                <Row>
+                  <Col xs={4}>
+                    <Input placeholder="TT" />
+                  </Col>
+                  <Col xs={4}>
+                    <Input placeholder="MM" />
+                  </Col>
+                  <Col xs={4}>
+                    <Input placeholder="JJJJ"/>
+                  </Col>
+                </Row>
+              </div>
+            </Col>
+          </Row>
+        </FormGroup>
 
-      <FormGroup>
-        <Row className="form-page__row">
-          <Col md={4} className="hidden-xs visible-md">
-            <FormLabel className="form-page__label">
-              Straße/Nr.*
-            </FormLabel>
-          </Col>
-          <Col md={6}>
-            <div className="form-page__inputs">
-              <Row>
-                <Col xs={8}>
-                  <FormLabel className="form-page__label hidden-md">
-                    Straße*
-                  </FormLabel>
-                  <Input placeholder="Straße" />
-                </Col>
-                <Col xs={4}>
-                  <FormLabel className="form-page__label hidden-md">
-                    Nr.*
-                  </FormLabel>
-                  <Input placeholder="Nr." />
-                </Col>
-              </Row>
-            </div>
-          </Col>
-        </Row>
-      </FormGroup>
+        <FormGroup>
+          <Row className="form-page__row">
+            <Col md={4} className="hidden-xs visible-md">
+              <FormLabel className="form-page__label">
+                Straße/Nr.*
+              </FormLabel>
+            </Col>
+            <Col md={6}>
+              <div className="form-page__inputs">
+                <Row>
+                  <Col xs={8}>
+                    <FormLabel className="form-page__label hidden-md">
+                      Straße*
+                    </FormLabel>
+                    <Input placeholder="Straße" />
+                  </Col>
+                  <Col xs={4}>
+                    <FormLabel className="form-page__label hidden-md">
+                      Nr.*
+                    </FormLabel>
+                    <Input placeholder="Nr." />
+                  </Col>
+                </Row>
+              </div>
+            </Col>
+          </Row>
+        </FormGroup>
 
-      <FormGroup>
-        <Row className="form-page__row">
-          <Col md={4} className="hidden-xs visible-md">
-            <FormLabel className="form-page__label">
-              PLZ/Ort*
-            </FormLabel>
-          </Col>
-          <Col md={6}>
-            <div className="form-page__inputs">
-              <Row>
-                <Col xs={4}>
-                  <FormLabel className="form-page__label hidden-md">
-                    PLZ
-                  </FormLabel>
-                  <Input placeholder="PLZ" />
-                </Col>
-                <Col xs={8}>
-                  <FormLabel className="form-page__label hidden-md">
-                    Ort*
-                  </FormLabel>
-                  <Input placeholder="Ort" />
-                </Col>
-              </Row>
-            </div>
-          </Col>
-        </Row>
-      </FormGroup>
+        <FormGroup>
+          <Row className="form-page__row">
+            <Col md={4} className="hidden-xs visible-md">
+              <FormLabel className="form-page__label">
+                PLZ/Ort*
+              </FormLabel>
+            </Col>
+            <Col md={6}>
+              <div className="form-page__inputs">
+                <Row>
+                  <Col xs={4}>
+                    <FormLabel className="form-page__label hidden-md">
+                      PLZ*
+                    </FormLabel>
+                    <Input placeholder="PLZ" />
+                  </Col>
+                  <Col xs={8}>
+                    <FormLabel className="form-page__label hidden-md">
+                      Ort*
+                    </FormLabel>
+                    <Input placeholder="Ort" />
+                  </Col>
+                </Row>
+              </div>
+            </Col>
+          </Row>
+        </FormGroup>
+      </form>
     </Container>
   );
 }

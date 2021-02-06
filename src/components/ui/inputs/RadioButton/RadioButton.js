b9937c5a85a3ba1a0fe1e219848c3ff0cec94088
DenisPuliaev <denis.puliaev@gmail.com>
2021-02-06 14:44:39 +0200

    fixed class names

--- src/components/ui/inputs/RadioButton/RadioButton.js
+++ src/components/ui/inputs/RadioButton/RadioButton.js
@@ -3,12 +3,12 @@ import classnames from "classnames";
 import "./radio.scss";
 
 const RadioButton = ({ form, className = '', children, invalid = false, ...rest }) => {
-  const classes = classnames({
+  const classes = classnames('radio', {
     'radio--invalid': invalid,
   });
 
   return (
-    <label className={`radio ${classes} ${className}`}>
+    <label className={`${classes} ${className}`}>
       <input className="radio__input" {...rest} />
       <span className="radio__checkmark"></span>
       {children}
--- src/components/ui/inputs/TextInput/TextInput.js
+++ src/components/ui/inputs/TextInput/TextInput.js
@@ -3,7 +3,7 @@ import classnames from "classnames";
 import "./input.scss";
 
 const TextInput = ({ form, className = '', type = "text", invalid = false,  ...rest }) => {
-  const classes = classnames({
+  const classes = classnames('input', {
     'input--invalid': invalid,
   });
 
@@ -11,7 +11,7 @@ const TextInput = ({ form, className = '', type = "text", invalid = false,  ...r
     <input
       {...rest}
       type={type}
-      className={`input ${classes} ${className}`}
+      className={`${classes} ${className}`}
     />
   );
 };
